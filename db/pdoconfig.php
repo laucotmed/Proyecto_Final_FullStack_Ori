e123f2901fc2fa05adb1b78015ae5cd4e73e6765
Laura Cote Medina <lauracotemedina@alumnos.cei.es>
2022-07-07 16:06:46 +0200

    Heroku Deploy

--- db/pdoconfig.php
+++ db/pdoconfig.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$dbname = "ProyectoFinalBBDD";
-$username = "laura";
-$password = "123";
+$host = "oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$dbname = "bwt2a5jrdcwbd4eo";
+$username = "kvsaritajkavi5ln";
+$password = "vh8wmyq9oews41ip";
 
 $options = [
     \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
