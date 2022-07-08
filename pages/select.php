8f6b4ad07b16c3a5c9d691c95624c4f78469adfb
Laura Cote Medina <lauracotemedina@alumnos.cei.es>
2022-07-08 15:55:02 +0200

    Prueba sacar datos

---
+++ pages/select.php
@@ -0,0 +1,20 @@
+<?php
+
+/* if(isset($_POST["btnEditar"])){ */
+
+    $id_skill = $_POST["item"];
+    
+    $skills = "SELECT * FROM skills WHERE skills.id=?";
+    
+        try{
+    
+        $connection->prepare($skills)->execute([$id_skill]);
+    
+        }catch(Exception $error){
+        echo $error;
+       
+    }
+    
+/*   } */
+
+?>
--- skills.php
+++ skills.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Etiquetas meta sobre el sitio web -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Proyecto Final para el módulo de Desarrollo Full Stack Developer de la Escuela de Diseño CEI. Temática: Videojuego Ori And the Blind Forest">
-    <meta name="keywords" content="Ori, Blind Forest, Videogame">
-    <meta name="robots" content="no-index, no-follow">
-    <meta name="language" content="English">
-    <meta name="author" content="Laura Cote Medina">
+  <!-- Etiquetas meta sobre el sitio web -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Proyecto Final para el módulo de Desarrollo Full Stack Developer de la Escuela de Diseño CEI. Temática: Videojuego Ori And the Blind Forest">
+  <meta name="keywords" content="Ori, Blind Forest, Videogame">
+  <meta name="robots" content="no-index, no-follow">
+  <meta name="language" content="English">
+  <meta name="author" content="Laura Cote Medina">
 
   <!-- Icono Favicon -->
   <link rel="shortcut icon" type="image/x-icon" href="src/img/ori.ico">
@@ -29,36 +29,42 @@
 </head>
 <body>
    
-    <!--  MENÚ PRINCIPAL -->
-    <nav class="menu">
-        <!-- Logo -->
-        <div id="menu_logo"><a href="index.php"><img src="src/img/logo.png" alt="Ori and the Blind Forest Logo.png"></a></div>
+  <!--  MENÚ PRINCIPAL -->
+  <nav class="menu">
+    <!-- Logo -->
+    <div id="menu_logo"><a href="index.php"><img src="src/img/logo.png" alt="Ori and the Blind Forest Logo.png"></a></div>
 
-        <!-- Menú hamburguesa que se mostrará cuando el tamaño sea lo suficientemente pequeño, formado por tres líneas en span -->
-        <div id="burger_menu">
-            <span class="burger-line"></span>
-            <span class="burger-line"></span>
-            <span class="burger-line"></span>
-        </div>
+    <!-- Menú hamburguesa que se mostrará cuando el tamaño sea lo suficientemente pequeño, formado por tres líneas en span -->
+    <div id="burger_menu">
+      <span class="burger-line"></span>
+      <span class="burger-line"></span>
+      <span class="burger-line"></span>
+    </div>
         
-          <!-- Links a las distintas páginas de la web -->
-        <ul class="menu_items">
-            <li class="menu_item"><a href="story.html">Story</a></li>
-            <li class="menu_item"><a href="characters.html">Characters</a></li>
-            <li class="menu_item active"><a href="skills.html">Skills</a></li>
-        </ul>
-    </nav>
+    <!-- Links a las distintas páginas de la web -->
+    <ul class="menu_items">
+      <li class="menu_item"><a href="story.html">Story</a></li>
+      <li class="menu_item"><a href="characters.html">Characters</a></li>
+      <li class="menu_item active"><a href="skills.html">Skills</a></li>
+    </ul>
+  </nav>
 
-    <!-- TARJETA DATOS HABILIDADES -->
+  <!-- TARJETA DATOS HABILIDADES -->
 
-    <section id="skills">
+  <section id="skills">
 
     <!-- Tarjeta con icono, título, descripción e imagen con cada habilidad-->
 
     <?php
 
+      /* con esto incluimos el código que se encuentra en connection.php y realizamos un echo para comprobar que la conexión es correcta */
       require_once "db/connection.php";
       /* echo "Conectado a la base de datos".$dbname." en el host ".$host; */
+
+      include "pages/select.php";
+      foreach($skills as $skill){
+        echo "<p>".$skill["nombre"]." - ".$skill["img"]."€ - ".$skill["control_consola"]."</p>";
+    }
 
     ?>
 
@@ -74,18 +80,20 @@
     </div>
 
     <div class="carousel-wrap">
-    <div class="slider owl-carousel owl-theme">
-          <div class="item"><img src="src/img/skills/SpiritFlame.png" alt="SpiritFlameIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/WallJump.png" alt="WallJumpIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/Bash.png" alt="BashIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/ChargeFlame.png" alt="ChargeFlameIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/ChargeJump.png" alt="ChargeJumpIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/Climb.png" alt="ClimbIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/DoubleJump.png" alt="DoubleJumpIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/Glide.png" alt="GlideIcon.png"></div>
-          <div  class="item"><img src="src/img/skills/Stomp.png" alt="Stomp.png"></div>
+      <div class="slider owl-carousel owl-theme">
+        <div class="item" value="1"><img src="src/img/skills/SpiritFlame.png" alt="SpiritFlameIcon.png"></div>
+        <div class="item"><img src="src/img/skills/WallJump.png" alt="WallJumpIcon.png"></div>
+        <div class="item"><img src="src/img/skills/ChargeFlame.png" alt="ChargeFlameIcon.png"></div>
+        <div class="item"><img src="src/img/skills/DoubleJump.png" alt="DoubleJumpIcon.png"></div>
+        <div class="item"><img src="src/img/skills/Bash.png" alt="BashIcon.png"></div>
+        <div class="item"><img src="src/img/skills/Stomp.png" alt="Stomp.png"></div>
+        <div class="item"><img src="src/img/skills/Glide.png" alt="GlideIcon.png"></div>
+        <div class="item"><img src="src/img/skills/Climb.png" alt="ClimbIcon.png"></div>
+        <div class="item"><img src="src/img/skills/ChargeJump.png" alt="ChargeJumpIcon.png"></div>
+        <div class="item"><img src="src/img/skills/Dash.png" alt="DashIcon.png"></div>
+        <div class="item"><img src="src/img/skills/LightBurst.png" alt="LightBurstIcon.png"></div>
+      </div>
     </div>
-  </div>
 
   </section>
   <script src="src/scripts/script.js"></script>
