883cad104d7d33c135ad599a6ae98df67116cadb
Laura Cote Medina <lauracotemedina@alumnos.cei.es>
2022-07-08 13:53:00 +0200

    Cambio de rutas en skills php

--- skills.php
+++ skills.php
@@ -12,16 +12,16 @@
     <meta name="author" content="Laura Cote Medina">
 
   <!-- Icono Favicon -->
-  <link rel="shortcut icon" type="image/x-icon" href="./src/img/ori.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="src/img/ori.ico">
 
   <!-- Enlaces a la hoja de estilo, al script y al CDN de Jquery-->
-  <link rel="stylesheet" href="./src/style/style.css">
+  <link rel="stylesheet" href="src/style/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="./src/scripts/owl.carousel.min.js"></script>
+  <script src="src/scripts/owl.carousel.min.js"></script>
 
-  <!-- Owl Stylesheets -->
-  <link rel="stylesheet" href="./src/style/owl.carousel.min.css">
-  <link rel="stylesheet" href="./src/style/owl.theme.default.min.css">
+  <!-- Owl Carousel Stylesheets -->
+  <link rel="stylesheet" href="src/style/owl.carousel.min.css">
+  <link rel="stylesheet" href="src/style/owl.theme.default.min.css">
 
   <!-- Título del sitio -->
   <title>Ori and the Blind Forest</title>
@@ -32,9 +32,9 @@
     <!--  MENÚ PRINCIPAL -->
     <nav class="menu">
         <!-- Logo -->
-        <div id="menu_logo"><a href="./index.php"><img src="src/img/logo.png" alt="Ori and the Blind Forest Logo.png"></a></div>
+        <div id="menu_logo"><a href="index.php"><img src="src/img/logo.png" alt="Ori and the Blind Forest Logo.png"></a></div>
 
-        <!-- Menú hamburguesa que se mostrará cuando el tamaño sea lo suficientemente pequeño -->
+        <!-- Menú hamburguesa que se mostrará cuando el tamaño sea lo suficientemente pequeño, formado por tres líneas en span -->
         <div id="burger_menu">
             <span class="burger-line"></span>
             <span class="burger-line"></span>
@@ -43,9 +43,9 @@
         
           <!-- Links a las distintas páginas de la web -->
         <ul class="menu_items">
-            <li class="menu_item"><a href="./story.html">Story</a></li>
-            <li class="menu_item"><a href="./characters.html">Characters</a></li>
-            <li class="menu_item active"><a href="./skills.html">Skills</a></li>
+            <li class="menu_item"><a href="story.html">Story</a></li>
+            <li class="menu_item"><a href="characters.html">Characters</a></li>
+            <li class="menu_item active"><a href="skills.html">Skills</a></li>
         </ul>
     </nav>
 
@@ -58,36 +58,36 @@
     <?php
 
       require_once "db/connection.php";
-      echo "Conectado a la base de datos".$dbname." en el host ".$host;
+      /* echo "Conectado a la base de datos".$dbname." en el host ".$host; */
 
     ?>
 
     <div id="card">
       <div id="profile">
-        <div id="img_skill"><img src="./src/img/skills/Stomp.png" alt="stomp.png"></div>
+        <div id="img_skill"><img src="src/img/skills/Stomp.png" alt="stomp.png"></div>
         <h1>STOMP</h1>
       </div>
       <div id="description">
-        <p>To use this ability hold <span><img src="./src/img/controls/SWButtonA.png" alt="buttonY.png"></span> / <span><img src="./src/img/controls/KeyboardButtonK.png" alt="buttonY.png"></span> near lanterns or enemy projectiles. then aim in the direction you'd like to leap. Projectiles will be redirected in the opposite direction</p>
-        <div id="description_img"><img src="./src/img/skills/SpiritFlame2.png" alt="SpiritFlame2.png"></div>
+        <p>To use this ability hold <span><img src="src/img/controls/SWButtonA.png" alt="buttonY.png"></span> / <span><img src="src/img/controls/KeyboardButtonK.png" alt="buttonY.png"></span> near lanterns or enemy projectiles. then aim in the direction you'd like to leap. Projectiles will be redirected in the opposite direction</p>
+        <div id="description_img"><img src="src/img/skills/SpiritFlame2.png" alt="SpiritFlame2.png"></div>
       </div>
     </div>
 
     <div class="carousel-wrap">
     <div class="slider owl-carousel owl-theme">
-          <div class="item"><img src="./src/img/skills/SpiritFlame.png" alt="SpiritFlameIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/WallJump.png" alt="WallJumpIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/Bash.png" alt="BashIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/ChargeFlame.png" alt="ChargeFlameIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/ChargeJump.png" alt="ChargeJumpIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/Climb.png" alt="ClimbIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/DoubleJump.png" alt="DoubleJumpIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/Glide.png" alt="GlideIcon.png"></div>
-          <div  class="item"><img src="./src/img/skills/Stomp.png" alt="Stomp.png"></div>
+          <div class="item"><img src="src/img/skills/SpiritFlame.png" alt="SpiritFlameIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/WallJump.png" alt="WallJumpIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/Bash.png" alt="BashIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/ChargeFlame.png" alt="ChargeFlameIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/ChargeJump.png" alt="ChargeJumpIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/Climb.png" alt="ClimbIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/DoubleJump.png" alt="DoubleJumpIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/Glide.png" alt="GlideIcon.png"></div>
+          <div  class="item"><img src="src/img/skills/Stomp.png" alt="Stomp.png"></div>
     </div>
   </div>
 
   </section>
-  <script src="./src/scripts/script.js"></script>
+  <script src="src/scripts/script.js"></script>
 </body>
 </html>
