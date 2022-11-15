04c3e8bdedbb2834b648017f8dde9819049b98f0
Laura Cote Medina <lauracotemedina@alumnos.cei.es>
2022-11-16 00:57:44 +0100

    test DB Railway

--- db/pdoconfig.php
+++ db/pdoconfig.php
@@ -2,10 +2,10 @@
 
 /* Estos son los datos necesarios para acceder a la base de datos (host, nombre de la base de datos, opciones de configuración...). Corresponden a los proporcionados por JawsDB en Heroku */
 
-$host = "oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$host = "containers-us-west-64.railway.app";
 $dbname = "bwt2a5jrdcwbd4eo";
-$username = "kvsaritajkavi5ln";
-$password = "vh8wmyq9oews41ip";
+$username = "root";
+$password = "railway";
 
 $options = [
     \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
